b34346b1e9015507469ba981ab3d34a1cc522f59
Winston Wu <pfunto.wu@gmail.com>
2019-09-06 23:39:51 -0700

    Update gitignore and config

--- frontend/config.js
+++ frontend/config.js
@@ -3,7 +3,10 @@ let wpUrl = 'http://localhost:8080/wp-json';
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
   wpUrl = 'http://wp-headless:8080/wp-json';
+} else {
+  wpUrl = 'http://api.kaiyros.net/wp-json';
 }
+
 const Config = {
   apiUrl: wpUrl,
   AUTH_TOKEN: 'auth-token',
